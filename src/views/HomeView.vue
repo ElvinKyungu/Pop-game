93ca2ea947bb0e03cf6e8b891d257ddab79ddd98
elvinkyungu <elvinkyungu.75@gmail.com>
2024-05-08 09:58:50 +0200

    feat: font configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-  <main>
-    
+  <main class="bg-[#2e20ac] flex justify-center h-screen w-full fixed">
+    <div class="absolute w-1/2 right-0 h-full bg-yellow-500">
+      <div class=" w-full h-full flex justify-center items-center">
+        <h2 class="font-pop">Pop</h2>
+        <h2 class="font-pacifico">Creative</h2>
+        <img src="@/assets/fruit.png" alt="" srcset="">
+      </div>
+    </div>
+    <div class="w-full h-full absolute justify-start items-end flex">
+      <img src="@/assets/air-ball.png" alt="" class="absolute -bottom-20 transform rotate-45" srcset="">
+    </div>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        pacifico: 'Pacifico',
+        pop: 'Poetsen One'
+      }
+    },
   },
   plugins: [],
 }
